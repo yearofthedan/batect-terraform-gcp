5ff187a717ebda5f8b855dbbdc8f2c9132f4e218
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2019-09-10 21:41:14 +1000

    Build and push image to gcp container registry

--- deploy/buildAndPushImage.sh
+++ deploy/buildAndPushImage.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env sh
 set -euo pipefail
 
-HEROKU_PROCESS_TYPE=web
+IMAGE_NAME=web
 TAG=$(git rev-parse HEAD)
-IMAGE_PATH=registry.heroku.com/"$WEB_APP"/"$HEROKU_PROCESS_TYPE"
+IMAGE_PATH=gcr.io/"$PROJECT_NAME"/"$IMAGE_NAME"
 
 docker build -t "$IMAGE_PATH":latest ./app
 docker tag      "$IMAGE_PATH":latest "$IMAGE_PATH":"$TAG"
-docker tag      "$IMAGE_PATH":latest "$IMAGE_PATH"
-docker login --username=_ --password="$HEROKU_API_KEY" registry.heroku.com
+
+# shellcheck disable=SC2002
+cat /secret/service-account.json | docker login -u _json_key --password-stdin https://gcr.io
 
 docker push "$IMAGE_PATH":latest
 docker push "$IMAGE_PATH":"$TAG"
-docker push "$IMAGE_PATH"

97b5f1954003c2acab7a08a93fe12a51f27ee270
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2019-09-15 15:38:02 +1000

    Split sed replaces to fix syntactic mistake

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,5 +12,5 @@ gcloud container clusters get-credentials "${CLUSTER_NAME}" --region us-west1-a
 
 echo Deploying update via kubectl for image: "$IMAGE_PATH"
 # shellcheck disable=SC2002
-cat ./deploy/app-deployment.yml | sed "s~{{IMAGE_PATH}}~$IMAGE_PATH|{{LB_STATIC_IP}}~$LB_STATIC_IP~g" | kubectl apply -f -
+cat ./deploy/app-deployment.yml | sed -e "s~{{IMAGE_PATH}}~$IMAGE_PATH~g" -e "s~{{LB_STATIC_IP}}~$LB_STATIC_IP~g" | kubectl apply -f -
 kubectl get service
